dba2f4a3483853bc65acea55734b4147e0511fba
Daniel Nogueira <danielsilvangr@gmail.com>
2021-06-29 13:22:10 -0300

    Add files via upload

---
+++ 2_average.py
@@ -0,0 +1,23 @@
+'''
+
+Programming Language: Python 3.9.4
+Developer: Daniel Nogueira
+
+Program: Average Results
+Based on: EIN420 Unity 2
+
+'''
+
+av1=float(input('Nota av1: '))
+av2=float(input('Nota av2: '))
+
+media=(av1+av2)/2
+print('Sua média foi ', media)
+
+if media>=5:
+	print('Aprovado')#identado, entao pertence ao if
+	if media>=8:
+		print('Parabéns pela grande nota')
+else:
+	print('Reprovado')
+	print('Estude mais')
---
+++ 2_looping.py
@@ -0,0 +1,25 @@
+'''
+
+Programming Language: Python 3.9.4
+Developer: Daniel Nogueira
+
+Program: Looping
+Based on: EIN420 Unity 2
+
+'''
+
+#5 execuções
+for i in range(5):
+	print(i)
+print('\n')
+
+#i inicializa automaticamente com valor 0
+for i in range(2):
+	for k in range(3):
+		print(i,k)
+print('\n')
+
+contador=7
+while contador<=10:
+	print('contador =', contador)
+	contador+=1
