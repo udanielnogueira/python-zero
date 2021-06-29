78ecbf713fb37dd911b664d643f76cf2fcd6e7d8
Daniel Nogueira <danielsilvangr@gmail.com>
2021-06-29 13:20:36 -0300

    Add files via upload

---
+++ 1_reading.py
@@ -0,0 +1,17 @@
+'''
+
+Programming Language: Python 3.9.4
+Developer: Daniel Nogueira
+
+Program: Reading Data
+Based on: EIN420 Unity 1
+
+'''
+
+funcao='Estudante'
+nome='Daniel Nogueira'
+print(funcao, nome)
+
+curso=input('Qual é o seu curso? ')
+print('Olá,', nome)
+print('Bem vindo ao curso de', curso)
